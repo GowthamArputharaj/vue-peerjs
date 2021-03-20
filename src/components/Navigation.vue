035f729e60278f334e85c3c462675c250a225ba4
GowthamArputharaj <gowtham.amateur.id@gmail.com>
2021-03-20 11:12:10 +0530

    video chat works

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,8 +2,13 @@
   <div id="navigation" class="">
     <nav class="navbar navbar-expand-lg navbar-light text-white text-monospace font-weight-bold">
       <router-link to="/" class="navbar-brand">Home</router-link>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
       <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto"> 
           <li class="nav-item rounded bg-warning ml-3">
             <router-link to="chat" class="nav-link">Chat</router-link>
           </li>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,45 +1,132 @@
 <template>
-  <div id="chat" class="container">
-    <div class="row">
-      <h1>This is an chat page</h1>
+  <div class="home" id="home">
+    <div>
+      <small>Welcome to <i>Media Call</i> {{ $store.state.name }}..</small>
     </div>
-    
-    <button type="button" class="btn btn-primary" @click="toggleModal" >
-      Launch demo modal
-    </button>
+
+    <div class="row mx-auto">
+      <div class="col-lg-4 col-md-6 col-sm-12 border border-primary pt-4 float-right">
+        <div class="form-group ">
+          <label for="connect_to">Connect to</label>
+          <input type="text" class="form-control" name="connect_to" v-model="connectTo" >
+        </div>
+        <div class="form-group ">
+          <label for="message">Message</label>
+          <textarea class="float-right" v-model="message" name="message" cols="30" rows="3" placeholder="Send some message" v-on:keyup.enter="connectCall()"></textarea>
+        </div>
+        <div class="form-group">
+          <button class="rounded btn btn-danger" @click="connectCall()">Connect</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mx-auto">
+      <h2 class="text-center text-monospace">Video here</h2>
+      <!-- <video v-bind:src="strm" width="300" height="200" controls></video> -->
+      <video id="other_stream" src="" width="300" height="200" controls></video>
+    </div>
+
+    <div class="row mx-auto">
+      <h2 class="text-center text-monospace">My Stream here</h2>
+      <!-- <video v-bind:src="strm" width="300" height="200" controls></video> -->
+      <video id="my_stream" src="" width="300" height="200" controls></video>
+    </div>
+
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import store from '../store'
 import Peer from 'peerjs';
 import { v4 as uuidv4 } from 'uuid';
+import Swal from 'sweetalert2'
 
 export default {
-  name: "Chat",
+  name: 'Home',
   data() {
     return {
-      uid: '',
+      uuid: '',
       name: '',
-      message: '',
-      type: '',
-      showModal: true,
+      peer: null,
+      connectTo: null,
+      message: null,
+      strm: null,
+      my_stream: null,
     }
+  },
+  created(){
+    this.uuid = uuidv4();
+
+    console.log('My Peer id is: \n', this.uuid);
+
+    const peer = new Peer(this.uuid);
+
+    // this.connectTo = history.length;
+
+    peer.on('call', async (call) => {
+      this.my_stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        console.log('this.my_stream');
+        console.log(this.my_stream);
+        call.answer(this.my_stream); // Answer the call with an A/V stream.
+        // document.querySelector('#my_stream').srcObject = stream;
+
+        call.on('stream', (remoteStream) => {
+          // Show stream in some <video> element.
+          console.log('RemoteStream is');
+          console.log(remoteStream);
+
+          document.querySelector('#other_stream').srcObject = remoteStream;
+          
+        });
+      // }, (err) => {
+      //   console.error('Failed to get local stream', err);
+      // });
+    });
+
+    this.peer = peer;
+
   },
   methods: {
-    toggleModal() {
-      this.showModal = !this.showModal;
-      console.log(this.showModal);
-    }
-  },
-  created() {
-    this.uid = uuidv4();
-  },
+    async connectCall() {
+      console.log('connectCall ()', this.connectTo);
 
+      try {
+        
+          console.log('stream connectCall()');
+          this.my_stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+          console.log(this.my_stream);
+          document.querySelector('#my_stream').srcObject = this.my_stream;
+  
+          const call = this.peer.call(this.connectTo, this.my_stream);
+  
+          call.on('stream', (remoteStream) => {
+            // Show stream in some <video> element.
+            console.log('remoteStream connectCall()')
+            console.log(remoteStream);
+  
+            document.querySelector('#other_stream').srcObject = remoteStream;
+  
+          });
+
+
+       
+        
+      } catch (error) {
+       console.log('ERROR') ;
+       console.log(error.message);
+      }
+      
+
+      console.log('end');
+    }
+  }
 }
 </script>
 
+
 <style scoped>
-#chat {
+#home {
   height: 90vh;
 }
 </style>
